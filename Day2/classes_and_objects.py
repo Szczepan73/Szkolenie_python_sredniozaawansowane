a2ba6552dfa8680af04efa0a37f333ebfb603043
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-12-03 15:17:50 +0100

    Klasy dziedziczenie

--- Day2/classes_and_objects.py
+++ Day2/classes_and_objects.py
@@ -141,7 +141,7 @@ print(f'Rectangle {r.dlugosc_a}, {r.b}')
 # wzor na bmi = masa / (wzrost ** 2)   wzrost podany w metrach 1.84
 # dodac metode __str__
 # Powołaj do życia obiekt tej klasy i wyświetl na konsoli obliczone BMI.
-
+@total_ordering
 class Zawodnik:
     def __init__(self, wzrost: float, masa: float, imie: str):
         self.__wzrost = wzrost
@@ -180,9 +180,22 @@ class Zawodnik:
 
     # odczytali dane z pliku dane.txt
     # zbudowali sobie liste zawodnikow (jako obietky klasy) przy uzyciu  @classmethod
+    @classmethod
+    def create_from_file(cls, path_to_file: str) -> list:
+        zawodnicy = []
+        with open(path_to_file, "r") as plik:
+            for linia in plik:
+                dane = linia.strip().split(";")
+                if len(dane) == 3:
+                    imie, waga, wzrost = dane
+                    zawodnicy.append(cls(masa=float(waga), wzrost=float(wzrost), imie=imie))
+        return zawodnicy
 
+    def __lt__(self, other):
+        return self.BMI < other.BMI
 
-
+    def __eq__(self, other):
+        return self.imie == other.imie
 
     # zła praktyka
     # def dodaj_atrybut_nazwisko(self, nazwisko:str):
@@ -208,16 +221,27 @@ z2.nie_uzywam_atrybutow("Hello world")
 z3 = Zawodnik.create_from_string("176;150;Paweł")
 print(z3)
 
+print("*" * 50)
 
 list_zawodnikow = Zawodnik.create_from_file("dane.txt")
 print(list_zawodnikow[0])
 
+print("*" * 50)
 
 
+lista_posortowana = sorted(list_zawodnikow, key= lambda zawodnik: zawodnik.BMI)
+for z in lista_posortowana:
+    print(z)
+
+print("*" * 50)
+
+list_zawodnikow.sort()
+
+for z in list_zawodnikow:
+    print(z)
 
 
-
-
+print(f"porownanie zawodnikow {list_zawodnikow[0] >= list_zawodnikow[1]}")
 
 
 
@@ -232,3 +256,364 @@ class MathUtils:
 
 
 MathUtils.add(2,2)
+
+
+
+
+# https://realpython.com/python-magic-methods/
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'Vector({self.x}, {self.y})'
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2D(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, scalar):
+        return Vector2D(self.x * scalar, self.y * scalar)
+
+    def __len__(self):
+        return 2
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else:
+            raise IndexError("Index out of range")
+
+    def __setitem__(self, index, value):
+        if index == 0:
+            self.x = value
+        elif index == 1:
+            self.y = value
+        else:
+            raise IndexError("Index out of range")
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+
+    def __contains__(self, value):
+        return value == self.x or value == self.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+v1 = Vector2D(3, 4)
+v2 = Vector2D(5, 6)
+
+print(repr(v1))
+print(str(v2))
+
+print(v1 == v2)  # __eq__
+print(v1 + v2)  # __add__: Vector(8, 10)
+print(v2 - v1)
+print(v1 * 2)
+
+print(len(v1))  # __len__: 2
+print(v1[0], v1[1])  # __getitem__: 3 4
+
+for coordinate in v2:  # __iter__
+    print(coordinate)
+
+print(5 in v2)  # __contains__
+
+hash_value = hash(v1)  # __hash__
+print(hash_value)
+
+v1[0] = 15  # __setitem__
+print(v1)
+
+
+
+
+# Napisz klase Timer która będzie mierzyła czas wykonania funkcji jako context menager.
+# klasa ta w zaleznosci od zmiennej verbose bedzie wypisywała na ekran czas wykonania przy wyjsciu z contextu
+# from timeit import default_timer as timer
+#timer_obj = timer
+#start = timer_obj()
+# cos co mierzymy
+#end = timer_obj()
+
+#def __enter__(self):
+#def __exit__(self, *args):
+
+class Timer:
+    """
+    Tu generalny opis do czego jest klasa
+    """
+    def __init__(self, verbose: bool):
+        """
+
+        Parameters
+        ----------
+        verbose
+        """
+        self.timer = timer
+        self.elapsed = 0
+        self.verbose = verbose
+        self.start = 0
+
+    def __enter__(self):
+        """
+
+        Returns
+        -------
+
+        """
+        print("Zaczynam pomiar")
+        self.start = self.timer()
+        return self
+
+    def __exit__(self, *args):
+        print("Koncze pomiar")
+        end = self.timer()
+        self.elapsed = end - self.start
+        self.elapsed *= 1000 # convert to miliseconds
+        if self.verbose:
+            print(f"Measured time: {self.elapsed}")
+
+
+
+
+
+with Timer(verbose=True) as t:
+    #time.sleep(3)
+    print("Moja bardzo długa funkcja")
+
+
+print(f'Ta funkcja trwała {t.elapsed}')
+
+
+
+# Dokumentowanie klas
+from pandas import DataFrame
+
+# https://www.sphinx-doc.org/en/master/
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
+#####################
+
+class BankAccount:
+    # Class variable to store interest rate
+    interest_rate = 0.05
+
+    def __init__(self, owner, balance):
+        self.owner = owner  # Instance variable
+        self.balance = balance
+
+    def calculate_interest(self):
+        self.balance += self.balance * BankAccount.interest_rate
+
+    def __str__(self):
+        return f'Account {self.owner} has {self.balance}'
+
+
+account1 = BankAccount("Alice", 1000)
+account2 = BankAccount("Bob", 2000)
+
+account1.calculate_interest()
+print(account1)
+print(BankAccount.interest_rate)  # Output: 0.05
+BankAccount.interest_rate = 0.1
+account2.calculate_interest()
+account1.calculate_interest()
+print(account1)
+print(account2)
+
+account1.interest_rate = 0.5
+account1.calculate_interest()
+print(account1)
+
+print(account1.interest_rate)
+print(account2.interest_rate)
+
+BankAccount.interest_rate = 0.2
+
+print(account1.interest_rate)
+print(account2.interest_rate)
+
+
+
+class Bag:
+    def __init__(self, items=[]):  # Unikaj mutowalnych domyślnych wartości
+        self.items = items
+
+bag1 = Bag()
+bag2 = Bag()
+
+bag1.items.append("book")
+print(bag1.items)  # ['book']
+print(bag2.items)  # ['book'] <- Oooo a co to - nieoczekiwane zachowanie
+
+
+@dataclass   # https://docs.python.org/3/library/dataclasses.html
+class Point:
+    x: int
+    y: int
+
+point = Point(x=10, y=20)
+print(point)
+
+
+@dataclass
+class Person:
+    first_name: str
+    last_name: str
+    age: int
+
+
+person1 = Person('Jan', 'Kowalski', 30)
+person2 = Person('Anna', 'Nowak', 25)
+
+print(person1)
+print(person2)
+
+print(person1 == person2)
+
+
+#################################################### CWICZENIE DODATKOWE
+
+# Stwórz klasę Ustawienia która będzie w momencie tworzenia obiektu czytac plik ustawienia.csv o treści:
+# encoding;utf-8
+# language;pl
+# timezone;-2
+# Dane te mają zostać wczytane do wewnętrznego słownika tak, by pierwsza kolumna stanowila klucze a druga wartosci.
+# Obiekt ma umożliwiać sprawdzanie ustawień w ten sposób:
+# u=Ustawienia()
+# print( u['encoding'] )
+# Obiekt ma umożliwiać też ustawienie wartości na zasadzie u[‘nazwa’]=’wartosc’.
+# W przypadku zmiany powinna ona dotyczyć również zawartości pliku.
+
+#################################################### CWICZENIE DODATKOWE
+
+
+
+class IDraw(ABC):
+    @abstractmethod
+    def drawing(self):
+        pass
+
+class Figure(ABC):
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def give_name(self):
+        print(f"Figure {self.name}, {self.color}")
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+
+class Squre(Figure, IDraw):
+    def __init__(self, lenght:float):
+        super().__init__("Squre", "red")
+        self.lenght = lenght
+
+    def area(self):
+        return self.lenght ** 2
+
+    def drawing(self):
+        print("**")
+        print("**")
+
+
+class Rectangle(Figure, IDraw):
+    def __init__(self, a:float, b:float, *args, **kwargs):
+        super().__init__("Rectangle", *args, **kwargs)
+        self.a = a
+        self.b = b
+
+    def area(self):
+        return self.a * self.b
+
+    def drawing(self):
+        print("****")
+        print("****")
+
+
+class SpecialRectangle(Rectangle):
+    def __init__(self, a: float, b: float, *args, **kwargs):
+        super().__init__(a,b, *args, **kwargs)
+
+    def area(self):
+        return self.a * self.b * 2
+
+    def drawing(self):
+        print("****" * 2)
+        print("****" * 2)
+
+
+# Figure("Nazwa", "color")
+
+figury =  [Rectangle(2,3, color="purple"), Squre(50), Rectangle(6,8, "red")]  # Figure("Nazwa") --- nie dozwolne tworzenie instancji klasy abstrakcyjnej
+for f in figury:
+    f.give_name()
+    print(f.area())
+    f.drawing()
+
+
+
+# zmien klase animal na klase abstrakcyjna
+# zmien metode speak na metode abstrakcyjna
+# dodaj interfejs ILiveOn ktore bedzie posiadał funkcje abstrakcyjna where_I_live()
+# dodac interfejs do klas pochodnych klasy animal
+
+
+class Animal():
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print("mowie")
+
+    def eat(self):
+        print("eating")
+
+    def __str__(self):
+        return f'My name is {self.name}'
+
+
+class Dog(Animal):
+    def speak(self):
+        print(f"{self.name} barks")
+
+
+class Cat(Animal):
+    def speak(self):
+        print(f"{self.name} meows")
+
+
+animal = Animal("Generic Animal") # !!!!
+dog = Dog("Buddy")
+cat = Cat("Whiskers")
+
+animal.speak() # !!!!
+dog.speak()
+cat.speak()
+cat.eat()
+
+lista_zwierzat = [cat, dog]
+
+for zwierze in lista_zwierzat:
+    zwierze.speak()
+    zwierze.where_I_live()
+    zwierze.eat()
+    print(zwierze)
