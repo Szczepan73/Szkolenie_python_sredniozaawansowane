cf7fe0afa9990c2d3d5466f1a6bce0caee6d29fa
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-12-03 09:52:18 +0100

    Dekoratory zadanie 3

--- Day2/decorators.py
+++ Day2/decorators.py
@@ -65,4 +65,75 @@ if __name__ == '__main__':
 
 
     # Napisz funkcje która przed i po wykonaniu innej funkcji wypisze 25 '*'     print("*" * 25)
+    def star(func):
+        def inner(*args, **kwargs):
+            print("*" * 25)
+            func(*args, **kwargs)
+            print("*" * 25)
 
+        return inner
+
+
+    def percent(func):
+        def inner(*args, **kwargs):
+            print("%" * 15)
+            func(*args, **kwargs)
+            print("%" * 15)
+
+        return inner
+
+
+    @percent
+    @star
+    def printer(msg: str):   # printer = percent(star(printer))
+        print(msg)
+
+
+    printer("Hello World")
+
+
+    @star
+    def printer_2(msg: str, count: int):
+        print(msg * count)
+
+    printer_2("Hello World", 2)
+
+
+    def hello_decorator(func):
+        def inner1(*args, **kwargs):
+            print("before Execution")
+
+            # getting the returned value
+            returned_value = func(*args, **kwargs)
+            print("after Execution")
+
+            # returning the value to the original frame
+            return returned_value
+
+        return inner1
+
+
+    @hello_decorator
+    def sum_two_numbers(a: int, b: int) -> int:          # sum_two_numbers = hello_decorator(sum_two_numbers)
+        print("Inside the function sum_two_numbers")
+        return a + b
+
+
+    result = sum_two_numbers(12, 4)
+    print(f"Wynik dodawania: 12 + 4 = {result}")
+
+
+    # Dodaj dekorator który zliczy czas wykonywania tej funkcji z parametrami. Zaloguj na konsole wszystkie przekazane parametry
+    
+
+
+
+    def opakuj_mnie_z_parametrami(x, napis_do_wypisania):
+        for i in range(x):
+            time.sleep(1)
+        print(f"Robie ciekawe rzeczy w Pythonie {napis_do_wypisania}")
+        return 10
+
+
+    returned_value = opakuj_mnie_z_parametrami(x=1, napis_do_wypisania="jestem cool")
+    print(returned_value)
