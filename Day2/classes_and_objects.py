e6206782752a6d674f193458cf8e5e45b6cc7785
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-12-03 10:29:06 +0100

    Dekoratory z parametrami zadanie

---
+++ Day2/classes_and_objects.py
@@ -0,0 +1,8 @@
+from timeit import default_timer as timer
+import time
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+import pandas as pd
+from functools import total_ordering   # fills in missing comparison methods (<, <=, >, >=) based on just two: either __eq__ and one other comparison method like __lt__ or __gt__.
--- Day2/decorators.py
+++ Day2/decorators.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import time
 import functools
 from typing import Callable
-
+import statistics
 
 if __name__ == '__main__':
     # https://refactoring.guru/pl/design-patterns/decorator
@@ -124,16 +124,90 @@ if __name__ == '__main__':
 
 
     # Dodaj dekorator który zliczy czas wykonywania tej funkcji z parametrami. Zaloguj na konsole wszystkie przekazane parametry
-    
+    def licz_czas_i_loguj(fun):
+        @functools.wraps(fun)
+        def wewnetrzna(*args, **kwargs):
+            print(f'Argumenty {args}')
+            print(f'Key-word arguments {kwargs}')
+            poczatek = datetime.now()
+            value = fun(*args, **kwargs)
+            koniec = datetime.now()
+            print(f'wywolanie trwalo {koniec - poczatek}')
+            return value
+
+        return wewnetrzna
 
 
+    @licz_czas_i_loguj
+    def opakuj_mnie_z_parametrami(x, napis_do_wypisania):    # opakuj_mnie_z_parametrami = licz_czas_i_loguj(opakuj_mnie_z_parametrami)
+        """
+        Ta funkcja spi przez x sekund a nastpenie wypisuje wiadomosc
 
-    def opakuj_mnie_z_parametrami(x, napis_do_wypisania):
+        Parameters
+        ----------
+        x : int powinien miec wartosc wieksza od 0
+        napis_do_wypisania
+
+        Returns
+        -------
+        Zawsze zwraca 10
+        """
         for i in range(x):
-            time.sleep(1)
+            time.sleep(0.1)
         print(f"Robie ciekawe rzeczy w Pythonie {napis_do_wypisania}")
         return 10
 
 
     returned_value = opakuj_mnie_z_parametrami(x=1, napis_do_wypisania="jestem cool")
     print(returned_value)
+
+    # print(opakuj_mnie_z_parametrami.__doc__)
+    help(opakuj_mnie_z_parametrami)
+    print(opakuj_mnie_z_parametrami.__name__)
+
+
+    def sleeper(sleep_time:float):
+        def sleeper_decorator(func):
+            @functools.wraps(func)
+            def inner(*args, **kwargs):
+                time.sleep(sleep_time)    # sleep_time = 2.5
+                returned_value = func(*args, **kwargs)
+                return returned_value
+
+            return inner
+
+        return sleeper_decorator
+
+
+    @sleeper(sleep_time=2.5)                          #  dekorator =  sleeper(sleep_time=2.5)
+                                                      #  slow_add = dekortor( slow_add )
+    def slow_add(a: float, b: float) -> float:
+        return a + b
+
+    print(f'Wynik dodawania 2+2=')
+    print(f'{slow_add(2, 2)}')
+
+
+    # Stwórz dekorator, który sprawdza, czy użytkownik ma odpowiednie uprawnienia do wywołania funkcji. Jeśli nie, wyświetli komunikat o braku dostępu.
+
+
+
+
+
+
+    @check_permissions('user')
+    def user_function(user):
+        print(f"Witaj, użytkowniku {user['name']}")
+
+
+    @check_permissions('admin')
+    def admin_function(user):
+        print("Witaj, adminie!")
+
+    user1 = {'name': 'Jan', 'permission': 'admin'}
+    user2 = {'name': 'Anna', 'permission': 'user'}
+
+    admin_function(user1)  # Output: Witaj, adminie!
+    admin_function(user2)  # Output: Brak uprawnień do wykonania tej funkcji
+    user_function(user2)  # Output: Witaj, użytkowniku!
+    user_function(user1)  # Output: Brak uprawnień do wykonania tej funkcji
