1a5d22f257a459613a8ce91057a1b48b013ca46c
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-12-04 14:21:51 +0100

    PyTest parametrize zadanie

--- Day3/PyTest_examples/code_pytest.py
+++ Day3/PyTest_examples/code_pytest.py
@@ -41,3 +41,30 @@ def calculate_average(numbers: list[float]) -> float | None:
     if not numbers:
         return None
     return sum(numbers) / len(numbers)
+
+
+def calculate_percentage(value: float, percent: float) -> float:
+    if percent < 0 or percent > 100:
+        raise ValueError("Percent must be between 0 and 100")
+    return value * (percent / 100)
+
+
+def nieprzetestowana_funckja(a):
+    if a > 10:
+        print(f"Hahahahaha a mnie nie przetestowałeś! {a}")
+    print(f"Testing")
+
+# pytest --cov=Day3 --cov-report=html
+# pytest -v
+# pytest -k "palindrome"
+# pytest -m podstawowe
+# pytest -s # przekierowanie std out na konsole
+
+
+# napiszcie testy z uzyciem dekoratora parametrize do tej funkcji
+def is_even(n):
+    """
+    Funkcja sprawdzająca, czy liczba jest parzysta.
+    Zwraca True, jeśli liczba jest parzysta, False w przeciwnym przypadku.
+    """
+    return n % 2 == 0
--- Day3/PyTest_examples/tests/test_code.py
+++ Day3/PyTest_examples/tests/test_code.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 
-from Day3.PyTest_examples.code_pytest import sumuj, dajCyfry, is_palindrome
+from Day3.PyTest_examples.code_pytest import sumuj, dajCyfry, is_palindrome, calculate_average, calculate_percentage
 
 def test_sumuj():
     assert sumuj(2,2) == 4
@@ -66,3 +66,53 @@ def test_palindrome_with_punctuation():
 def test_palindrome_with_numbers():
     assert is_palindrome("12321") == True
     assert is_palindrome("12345") == False
+
+
+
+def test_average_empty_list():
+    assert calculate_average([]) is None
+
+
+def test_average_single_case():
+    assert calculate_average([1, 2, 3]) == 2.0
+
+# def test_average_single_case_2():
+#     assert calculate_average([1, 2, 3 ,2]) == 2.0
+
+
+lista_przypadkow_testowych = ["hello", "python", "123456x"]
+@pytest.mark.parametrize('text', lista_przypadkow_testowych)
+def test_palindrome_false(text):
+    assert is_palindrome(text) == False
+
+
+lista_przypadkow_testowych2 = [("hello", False), ("python", False), ("123456x", False), ("Kajak", True)]
+@pytest.mark.parametrize('text, result', lista_przypadkow_testowych2)
+def test_palindrome_false_parametryzacja_z_wynikiem(text, result):
+    assert is_palindrome(text) == result
+
+
+@pytest.mark.parametrize("numbers, expected_result", [
+    ([1, 2, 3], 2.0),
+    ([0, 0, 0, 0], 0.0),
+    ([10, 20, 30, 40, 50], 30.0),
+])
+def test_average_parametrized(numbers, expected_result):
+    print("Hello world")
+    assert calculate_average(numbers) == expected_result
+
+
+
+def test_calculate_percentage_invalid_percent():
+    with pytest.raises(ValueError) as e:
+        calculate_percentage(100, -10)
+    assert str(e.value) == "Percent must be between 0 and 100"
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="Pomijany na Windows")
+def test_not_on_windows():
+    assert True
+
+@pytest.mark.skipif(sys.version_info < (3, 9), reason="Wymaga Pythona 3.9 lub wyższej")
+def test_requires_python_39():
+    assert True
