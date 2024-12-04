37f1ae35b94b7e96d1e09cdcf85df0a4353debce
Wojciech Dudzik <wojtekdudzik@o2.pl>
2024-12-04 10:42:28 +0100

    Generator nieskonczony zadanie

--- Day3/generators.py
+++ Day3/generators.py
@@ -83,3 +83,19 @@ if __name__ == '__main__':
     print(next(genrator))
     next(genrator)
     print(next(genrator))
+
+
+
+    def is_prime(num):
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+
+    # napisz generator ktory bedzie zwracał nieskonczenie wiele liczb pierwszych
+
+
+
